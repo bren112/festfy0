ad5f481969c60d99bfe14c4df2756961a78877cc
vitor.pro@outlook.com <vitor.pro@outlook.com>
2023-11-18 16:03:44 -0300

    att

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,17 +1,32 @@
 import { Link } from "react-router-dom";
 import style from "./Header.module.css";
+import { useState } from "react";
 function Header() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <header className={style.header}>
       <Link to="/">
         <span>vitorsilva.dev </span>
       </Link>
-      <nav>
+      <nav
+        onClick={toggleMenu}
+        className={`${style.links} ${showMenu ? style.show : ""}`}
+      >
         <Link to="/">Home</Link>
         <Link to="/sobre">Sobre</Link>
         <Link to="/projetos">Projetos</Link>
         <Link to="/contatos">Contatos</Link>
       </nav>
+      <div onClick={toggleMenu} className={style.menuButton}>
+        <span className={style.menuItem}></span>
+        <span className={style.menuItem}></span>
+        <span className={style.menuItem}></span>
+      </div>
     </header>
   );
 }
